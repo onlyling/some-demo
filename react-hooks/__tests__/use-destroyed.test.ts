874455d160a7676932c711d7cc4eff7e9e7e9fd8
LING_ZI_QING <onlylingstudio@gmail.com>
2021-03-26 11:08:08 +0800

    tests: useDestroyed

---
+++ react-hooks/__tests__/use-destroyed.test.ts
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useDestroyed from '../src/use-destroyed';
+
+describe('useDestroyed', () => {
+  test('test mount', () => {
+    const { result } = renderHook(() => useDestroyed());
+
+    expect(result.current()).toBe(false);
+  });
+
+  test('test unmounted', async () => {
+    const { unmount, result } = renderHook(() => useDestroyed());
+    unmount();
+    expect(result.current()).toBe(true);
+  });
+});

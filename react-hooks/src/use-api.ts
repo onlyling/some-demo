6aab4e39f96e105d12c70e53636459a68d51e084
LING_ZI_QING <onlylingstudio@gmail.com>
2021-03-26 14:19:24 +0800

    feat: useAPI

--- react-hooks/src/use-api.ts
+++ react-hooks/src/use-api.ts
@@ -1,7 +1,8 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import useOriginalDeepCopy from './previous-value/use-original-deep-copy';
 import useState from './use-state';
+import * as helper from './helpers';
 
 /**
  * 接口约定的基本参数
@@ -26,21 +27,26 @@ type LocalState<T> = {
 
 const defaultParams = {};
 
+const defaultOption = {
+  manual: false,
+  defaultParams,
+};
+
 /**
  * 单个请求
  */
 const useAPI = <T, PT = Record<string, any>>(
   initValue: T,
-  fetchAPI: (p: any) => Promise<BaseResponse<T>>,
-  option: UseAPIOption<PT> = {
-    manual: false,
-    defaultParams: defaultParams as PT,
-  },
+  fetchAPI: (p?: any) => Promise<BaseResponse<T>>,
+  option: UseAPIOption<PT> = defaultOption as UseAPIOption<PT>,
 ) => {
   /** 是否手动执行 */
   const manual = option.manual;
+
   /** 自动执行的可变参数 */
   const defaultParams = useOriginalDeepCopy(option.defaultParams);
+
+  const fetchTime = useRef(0);
 
   const [localState, setLocalState] = useState<LocalState<T>>({
     loading: option.loading || !option.manual,
@@ -48,8 +54,53 @@ const useAPI = <T, PT = Record<string, any>>(
     data: initValue,
   });
 
+  const run = useCallback(
+    (p?): Promise<void> => {
+      setLocalState({
+        loading: true,
+        fail: false,
+      });
+
+      const time = new Date().getTime();
+      fetchTime.current = time;
+
+      return new Promise((resolve, reject) => {
+        fetchAPI(p)
+          .then(({ data }) => {
+            if (fetchTime.current === time) {
+              setLocalState({
+                loading: false,
+                fail: false,
+                data: data,
+              });
+            }
+
+            resolve();
+          })
+          .catch(() => {
+            if (fetchTime.current === time) {
+              setLocalState({
+                loading: false,
+                fail: true,
+              });
+            }
+            reject();
+          });
+      });
+    },
+    [fetchAPI],
+  );
+
+  useEffect(() => {
+    if (!manual) {
+      run(defaultParams).catch(helper.noop);
+    }
+  }, [manual, run, defaultParams]);
+
   return {
     ...localState,
+    run,
+    setState: setLocalState,
   };
 };
 
